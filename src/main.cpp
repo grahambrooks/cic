b67839ee1e2c423d5fa5a18b0452b9d3f083ad74
Graham Brooks <graham@grahambrooks.com>
2013-09-26 22:31:07 -0400

    Initial bootstrapping code

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ci
+//
+//  Created by Graham Brooks on 9/26/13.
+//  Copyright (c) 2013 Graham Brooks. All rights reserved.
+//
+
+#include <iostream>
+
+#include "tool.hpp"
+
+int main(int argc, const char * argv[])
+{
+    ci::tool tool;
+    
+    return tool.run();
+}
---
+++ src/tool.hpp
@@ -0,0 +1,10 @@
+namespace ci {
+    class tool {
+    public:
+        int run() {
+            std::cout << "ci v0.0.1 - continuous integration command line tool" << std::endl;
+            
+            return 0;
+        }
+    };
+}
