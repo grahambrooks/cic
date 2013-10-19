a01c7a7fae91950fa9da6bf1fb0e9ff919aeda07
Graham Brooks <graham@grahambrooks.com>
2013-10-19 17:18:18 -0400

    Jobs have names

--- test/job_test.cpp
+++ test/job_test.cpp
@@ -8,25 +8,36 @@ namespace ci {
       unknown,
       successful
     } job_status;
-
-    job(status_type status = unknown) : job_status(status) {
+    std::string name;
+    job(std::string name, status_type status = unknown) : name(name), job_status(status) {
     }
 
     status_type status() {
       return job_status;
+    }
+
+    std::string get_name() {
+      return name;
     }
   };
 }
 
 BOOST_AUTO_TEST_CASE(job_default_status_is_unknown)
 {
-  ci::job job;
+  ci::job job("");
 
   BOOST_CHECK_EQUAL(job.status(), ci::job::status_type::unknown);
 }
 
 BOOST_AUTO_TEST_CASE(status_set_during_creation) {
-  ci::job job(ci::job::status_type::successful);
+  ci::job job("", ci::job::status_type::successful);
 
   BOOST_CHECK_EQUAL(job.status(), ci::job::status_type::successful);
+}
+
+BOOST_AUTO_TEST_CASE(jobs_are_named) {
+  ci::job job("foo");
+
+  BOOST_CHECK_EQUAL(job.get_name(), "foo");
+
 }
