b98daf34e58d91ac990403bc073cc2d064cec8cb
Graham Brooks <graham@grahambrooks.com>
2013-10-19 20:31:32 -0400

    Added job properties but I am not conviced that this is the right way to go.

--- test/job_test.cpp
+++ test/job_test.cpp
@@ -1,4 +1,5 @@
 #include <boost/test/unit_test.hpp>
+#include <map>
 
 namespace ci {
 
@@ -10,6 +11,8 @@ namespace ci {
     } job_status;
     std::string name;
     std::list<std::string> artifacts;
+    std::map<std::string, std::string> properties;
+
     job(std::string name, status_type status = unknown) : name(name), job_status(status) {
     }
 
@@ -21,8 +24,21 @@ namespace ci {
       return name;
     }
 
-    const std::list<std::string>& get_artifacts() {
+    const std::list<std::string> &get_artifacts() {
       return artifacts;
+    }
+
+    job &operator ()(std::string key, std::string value) {
+      properties[key] = value;
+      return *this;
+    }
+
+    std::string get(std::string key) {
+      return properties[key];
+    }
+
+    job &add() {
+      return *this;
     }
   };
 }
@@ -50,4 +66,15 @@ BOOST_AUTO_TEST_CASE(jobs_have_artifacts) {
   ci::job job("foo");
 
   BOOST_CHECK_EQUAL(job.get_artifacts().size(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(jobs_have_properties) {
+  ci::job job("foo");
+  job.add()
+          ("shortDescription", "A job")
+          ("userId", "a user")
+          ("userName", "user name")
+          ("claimedBy", "me");
+
+  BOOST_CHECK_EQUAL(job.get("shortDescription"), "A job");
 }
